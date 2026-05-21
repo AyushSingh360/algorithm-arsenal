7cde1be0784bb6c60bd1d7b0b3b1e63453238419
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-21 01:44:24 +0000

    [LeetCode Sync] - Find the Length of the Longest Common Prefix - Runtime - 209 ms (77.12%), Memory - 29.7 MB (91.69%)

---
+++ leetcode/3329-find-the-length-of-the-longest-common-prefix/solution.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        # Store all prefixes of numbers from arr1
+        prefixes = set()
+
+        for x in arr1:
+            num = x
+            while num > 0:
+                prefixes.add(num)
+                num //= 10  # strip last digit, e.g. 1234 -> 123 -> 12 -> 1
+
+        ans = 0
+
+        # For each number in arr2, check its prefixes from longest to shortest
+        for y in arr2:
+            num = y
+            while num > 0:
+                if num in prefixes:
+                    # Length of prefix is number of digits
+                    ans = max(ans, len(str(num)))
+                    break  # no need to check smaller prefixes of this y
+                num //= 10
+
+        return ans
