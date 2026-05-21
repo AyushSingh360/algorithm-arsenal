83be33bf8dd2f23c2fe33f8459b0d4475c5cb913
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-21 01:46:03 +0000

    [LeetCode Sync] - Two Best Non-Overlapping Events - Runtime - 174 ms (56.02%), Memory - 67.2 MB (63.00%)

---
+++ leetcode/2164-two-best-non-overlapping-events/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+from bisect import bisect_right
+
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        events.sort()
+        n = len(events)
+
+        starts = [e[0] for e in events]
+        suf = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suf[i] = max(suf[i + 1], events[i][2])
+
+        ans = 0
+        for s, e, v in events:
+            j = bisect_right(starts, e)
+            ans = max(ans, v + suf[j], v)
+
+        return ans
