331534d6da32c2dc96c0905be2fbf8c0cbcd5409
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-25 02:02:23 +0000

    [LeetCode Sync] - Jump Game VII - Runtime - 167 ms (54.05%), Memory - 20.5 MB (96.46%)

---
+++ leetcode/2001-jump-game-vii/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        if s[-1] == '1':
+            return False
+        
+        reachable = [False] * n
+        reachable[0] = True
+        prefix = 0
+        
+        for i in range(1, n):
+            left = i - maxJump
+            right = i - minJump
+            
+            if right >= 0:
+                if reachable[right]:
+                    prefix += 1
+                
+                if left - 1 >= 0 and reachable[left - 1]:
+                    prefix -= 1
+                
+                if s[i] == '0' and prefix > 0:
+                    reachable[i] = True
+        
+        return reachable[-1]
