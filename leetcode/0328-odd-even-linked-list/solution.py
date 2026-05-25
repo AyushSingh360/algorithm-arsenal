ff93e4da3d8f6f7c643149d3c6bd9220cfa7a35d
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-25 14:32:26 +0000

    [LeetCode Sync] - Odd Even Linked List - Runtime - 0 ms (100.00%), Memory - 21 MB (75.88%)

---
+++ leetcode/0328-odd-even-linked-list/solution.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        odd = head          # head of odd-indexed list (1st, 3rd, 5th, ...)
+        even = head.next    # head of even-indexed list (2nd, 4th, 6th, ...)
+        even_head = even    # save to attach at the end
+
+        while even and even.next:
+            # Link odd to the next odd node
+            odd.next = even.next
+            odd = odd.next
+
+            # Link even to the next even node
+            even.next = odd.next
+            even = even.next
+
+        # Attach even list after the last odd node
+        odd.next = even_head
+
+        return head
