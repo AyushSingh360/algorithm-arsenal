cc0f2365c42261fd668ebb59f98dc638324f58a4
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-27 02:18:03 +0000

    [LeetCode Sync] - Count the Number of Special Characters II - Runtime - 326 ms (36.47%), Memory - 21.4 MB (62.94%)

---
+++ leetcode/3405-count-the-number-of-special-characters-ii/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        # last position of lowercase, first position of uppercase
+        lastLower = [-1] * 26
+        firstUpper = [len(word)] * 26
+
+        # record positions
+        for i, ch in enumerate(word):
+            if 'a' <= ch <= 'z':
+                idx = ord(ch) - ord('a')
+                lastLower[idx] = i
+            else:  # 'A' <= ch <= 'Z'
+                idx = ord(ch) - ord('A')
+                if firstUpper[idx] == len(word):
+                    firstUpper[idx] = i
+
+        # count special letters
+        ans = 0
+        for i in range(26):
+            if lastLower[i] != -1 and firstUpper[i] != len(word) and lastLower[i] < firstUpper[i]:
+                ans += 1
+
+        return ans
