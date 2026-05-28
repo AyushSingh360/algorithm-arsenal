24305340e673bff5462531cdc7c46e70c528175f
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-28 01:26:59 +0000

    [LeetCode Sync] - Design Twitter - Runtime - 13 ms (60.58%), Memory - 28.4 MB (77.62%)

---
+++ leetcode/0355-design-twitter/solution.py
@@ -0,0 +1,43 @@
+from typing import List
+from collections import defaultdict
+import heapq
+
+class Twitter:
+
+    def __init__(self):
+        self.time = 0
+        self.following = defaultdict(set)
+        self.tweets = defaultdict(list)   # userId -> [(time, tweetId), ...]
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.time += 1
+        self.tweets[userId].append((self.time, tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        heap = []
+        users = set(self.following[userId])
+        users.add(userId)
+
+        for uid in users:
+            if self.tweets[uid]:
+                idx = len(self.tweets[uid]) - 1
+                time, tweetId = self.tweets[uid][idx]
+                heapq.heappush(heap, (-time, tweetId, uid, idx - 1))
+
+        feed = []
+        while heap and len(feed) < 10:
+            neg_time, tweetId, uid, next_idx = heapq.heappop(heap)
+            feed.append(tweetId)
+
+            if next_idx >= 0:
+                time, next_tweetId = self.tweets[uid][next_idx]
+                heapq.heappush(heap, (-time, next_tweetId, uid, next_idx - 1))
+
+        return feed
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.following[followerId].discard(followeeId)
