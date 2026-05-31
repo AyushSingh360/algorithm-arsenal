3fcbc9bba299bb247f5c083df5f4fb3e056856a7
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-31 02:09:11 +0000

    [LeetCode Sync] - Destroying Asteroids - Runtime - 67 ms (88.48%), Memory - 34.2 MB (6.33%)

---
+++ leetcode/2245-destroying-asteroids/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        
+        # Use a larger integer type if language requires; in Python int is unbounded
+        cur_mass = mass
+        
+        for a in asteroids:
+            if cur_mass < a:
+                return False
+            cur_mass += a
+        
+        return True
