59ae92847c4d2cb53617654e01db8ef336f16df3
AyushSingh360 <ayushsingh21109@gmail.com>
2026-06-16 02:06:16 +0000

    [LeetCode Sync] - Process String with Special Operations I - Runtime - 47 ms (18.42%), Memory - 28.3 MB (29.61%)

---
+++ leetcode/3931-process-string-with-special-operations-i/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        res = []
+
+        for ch in s:
+            if 'a' <= ch <= 'z':
+                # Append lowercase letter
+                res.append(ch)
+            elif ch == '*':
+                # Remove last character if it exists
+                if res:
+                    res.pop()
+            elif ch == '#':
+                # Duplicate the current result and append
+                res = res + res
+            elif ch == '%':
+                # Reverse the current result
+                res.reverse()
+
+        return ''.join(res)
