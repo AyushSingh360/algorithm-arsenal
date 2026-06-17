f142d48ac2f772ffe0b14b48e4f58497b7423219
AyushSingh360 <ayushsingh21109@gmail.com>
2026-06-17 15:50:42 +0000

    [LeetCode Sync] - Random Pick Index - Runtime - 57 ms (12.85%), Memory - 29.9 MB (30.42%)

---
+++ leetcode/0398-random-pick-index/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+import random
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.indices_map = {}
+        for i, n in enumerate(nums):
+            if n not in self.indices_map:
+                self.indices_map[n] = []
+            self.indices_map[n].append(i)
+
+    def pick(self, target: int) -> int:
+        return random.choice(self.indices_map[target])
