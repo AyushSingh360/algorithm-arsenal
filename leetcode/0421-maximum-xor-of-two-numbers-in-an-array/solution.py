a4a7e78c143131930eb963aba6e3e337fc0e6f2c
AyushSingh360 <ayushsingh21109@gmail.com>
2026-06-23 15:48:23 +0000

    [LeetCode Sync] - Maximum XOR of Two Numbers in an Array - Runtime - 5657 ms (12.39%), Memory - 458.4 MB (33.25%)

---
+++ leetcode/0421-maximum-xor-of-two-numbers-in-an-array/solution.py
@@ -0,0 +1,38 @@
+class TrieNode:
+    def __init__(self):
+        self.child = [None, None]
+
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        root = TrieNode()
+
+        def insert(x: int) -> None:
+            node = root
+            for i in range(31, -1, -1):
+                b = (x >> i) & 1
+                if not node.child[b]:
+                    node.child[b] = TrieNode()
+                node = node.child[b]
+
+        def query(x: int) -> int:
+            node = root
+            ans = 0
+            for i in range(31, -1, -1):
+                b = (x >> i) & 1
+                want = 1 - b
+                if node.child[want]:
+                    ans = (ans << 1) | 1
+                    node = node.child[want]
+                else:
+                    ans = ans << 1
+                    node = node.child[b]
+            return ans
+
+        for num in nums:
+            insert(num)
+
+        best = 0
+        for num in nums:
+            best = max(best, query(num))
+
+        return best
