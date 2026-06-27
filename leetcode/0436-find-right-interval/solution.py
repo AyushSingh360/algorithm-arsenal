66312cc38428d50affbf9dc59c91b80338802ad9
AyushSingh360 <ayushsingh21109@gmail.com>
2026-06-27 01:50:46 +0000

    [LeetCode Sync] - Find Right Interval - Runtime - 19 ms (89.35%), Memory - 24.5 MB (38.66%)

---
+++ leetcode/0436-find-right-interval/solution.py
@@ -0,0 +1,15 @@
+from bisect import bisect_left
+from typing import List
+
+class Solution:
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        n = len(intervals)
+        starts = sorted(iv[0] for iv in intervals)
+        pos = {iv[0]: i for i, iv in enumerate(intervals)}
+        ans = [-1] * n
+
+        for i, iv in enumerate(intervals):
+            j = bisect_left(starts, iv[1])
+            if j < n:
+                ans[i] = pos[starts[j]]
+        return ans
