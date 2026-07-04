e49760785fec7ce294da21fbfe5a18af90eb1b7a
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-04 01:29:07 +0000

    [LeetCode Sync] - Serialize and Deserialize BST - Runtime - 63 ms (74.00%), Memory - 22.5 MB (72.62%)

---
+++ leetcode/0449-serialize-and-deserialize-bst/solution.py
@@ -0,0 +1,38 @@
+from typing import Optional, List
+
+class Codec:
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        vals = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if not node:
+                continue
+            vals.append(str(node.val))
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return ",".join(vals)
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if not data:
+            return None
+        nums = list(map(int, data.split(",")))
+        n = len(nums)
+        idx = 0
+
+        def build(lower, upper):
+            nonlocal idx
+            if idx >= n:
+                return None
+            val = nums[idx]
+            if val < lower or val > upper:
+                return None
+            idx += 1
+            node = TreeNode(val)
+            node.left = build(lower, val - 1 if isinstance(val, int) else val)
+            node.right = build(val + 1 if isinstance(val, int) else val, upper)
+            return node
+
+        return build(-10**9, 10**9)
