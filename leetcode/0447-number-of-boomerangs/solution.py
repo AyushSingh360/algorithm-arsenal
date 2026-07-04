05ba1d507c0e6cd1f9a230df4659701e095e257c
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-04 01:26:51 +0000

    [LeetCode Sync] - Number of Boomerangs - Runtime - 627 ms (12.07%), Memory - 19.4 MB (68.32%)

---
+++ leetcode/0447-number-of-boomerangs/solution.py
@@ -0,0 +1,13 @@
+import collections
+from typing import List
+
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        ans = 0
+        for x1, y1 in points:
+            count = collections.Counter()
+            for x2, y2 in points:
+                d = (x1 - x2) ** 2 + (y1 - y2) ** 2
+                ans += 2 * count[d]
+                count[d] += 1
+        return ans
