5965af128384e6f555087d29bc7fd76385dd2f43
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-05 01:34:50 +0000

    [LeetCode Sync] - Sort Characters By Frequency - Runtime - 7 ms (79.94%), Memory - 20.1 MB (64.97%)

---
+++ leetcode/0451-sort-characters-by-frequency/solution.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        cnt = Counter(s)
+        # sort characters by frequency descending; tie order doesn't matter
+        chars = sorted(cnt.items(), key=lambda kv: -kv[1])
+        return ''.join(ch * freq for ch, freq in chars)
