c274072a4103ab3973768c3b8978629f1b4b3f26
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-13 02:12:50 +0000

    [LeetCode Sync] - Sequential Digits - Runtime - 0 ms (100.00%), Memory - 19.3 MB (30.59%)

---
+++ leetcode/1212-sequential-digits/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        res = []
+        low_len, high_len = len(str(low)), len(str(high))
+
+        # lengths that matter
+        for length in range(low_len, high_len + 1):
+            # starting digit; must leave room for 'length' digits up to 9
+            for start in range(1, 10 - length + 1):
+                num = 0
+                # build a sequential-digit number of this length
+                for i in range(length):
+                    num = num * 10 + (start + i)
+                if low <= num <= high:
+                    res.append(num)
+
+        res.sort()
+        return res
