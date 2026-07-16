a6365b7c90877ddeed8594efb968dfeaf98ce2d5
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-16 01:34:47 +0000

    [LeetCode Sync] - Sum of GCD of Formed Pairs - Runtime - 180 ms (80.24%), Memory - 33.5 MB (85.63%)

---
+++ leetcode/4242-sum-of-gcd-of-formed-pairs/solution.py
@@ -0,0 +1,19 @@
+from math import gcd
+
+class Solution:
+    def gcdSum(self, nums: list[int]) -> int:
+        n = len(nums)
+        prefix_gcd = [0] * n
+
+        mx = 0
+        for i, x in enumerate(nums):
+            mx = max(mx, x)
+            prefix_gcd[i] = gcd(x, mx)
+
+        prefix_gcd.sort()
+
+        ans = 0
+        for i in range(n // 2):
+            ans += gcd(prefix_gcd[i], prefix_gcd[n - 1 - i])
+
+        return ans
