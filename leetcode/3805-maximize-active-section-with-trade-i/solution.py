b7701c60f61c4186c9bd9d8a5852e3164479e6dc
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-21 01:17:27 +0000

    [LeetCode Sync] - Maximize Active Section with Trade I - Runtime - 557 ms (86.92%), Memory - 20.6 MB (87.85%)

---
+++ leetcode/3805-maximize-active-section-with-trade-i/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+        from math import inf
+
+        n = len(s)
+        ans = 0
+        i = 0
+        pre = -inf  # previous zero-run length
+        mx = 0      # best sum of two adjacent zero runs
+
+        while i < n:
+            j = i + 1
+            # find end of current run
+            while j < n and s[j] == s[i]:
+                j += 1
+            cur = j - i  # length of this run
+
+            if s[i] == '1':
+                # add all ones directly
+                ans += cur
+            else:
+                # update best adjacent-zero sum
+                mx = max(mx, pre + cur)
+                pre = cur
+
+            i = j
+
+        # add best gain from one trade
+        ans += mx
+        return ans
