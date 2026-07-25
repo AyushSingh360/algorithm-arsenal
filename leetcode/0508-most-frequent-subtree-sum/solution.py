d614dcb6e89277f4f3d98b59e4eb5f1991144ef7
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-25 16:38:26 +0000

    [LeetCode Sync] - Most Frequent Subtree Sum - Runtime - 3 ms (75.73%), Memory - 21.5 MB (35.67%)

---
+++ leetcode/0508-most-frequent-subtree-sum/solution.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from typing import List, Optional
+
+class Solution:
+    def findFrequentTreeSum(self, root: Optional[TreeNode]) -> List[int]:
+        freq = Counter()
+
+        def dfs(node):
+            if not node:
+                return 0
+            total = node.val + dfs(node.left) + dfs(node.right)
+            freq[total] += 1
+            return total
+
+        dfs(root)
+        max_freq = max(freq.values())
+        return [s for s, c in freq.items() if c == max_freq]
