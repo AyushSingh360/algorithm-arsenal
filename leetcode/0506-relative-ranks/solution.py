ead1c745cbc748da4c579f65230b5423dc7fb154
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-25 16:37:23 +0000

    [LeetCode Sync] - Relative Ranks - Runtime - 3 ms (91.90%), Memory - 20 MB (97.09%)

---
+++ leetcode/0506-relative-ranks/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        n = len(score)
+        ans = [""] * n
+        order = sorted(range(n), key=lambda i: score[i], reverse=True)
+
+        medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+
+        for place, idx in enumerate(order):
+            if place < 3:
+                ans[idx] = medals[place]
+            else:
+                ans[idx] = str(place + 1)
+
+        return ans
