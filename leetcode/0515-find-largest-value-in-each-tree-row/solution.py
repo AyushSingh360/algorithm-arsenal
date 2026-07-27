0a27d9664d09ea7c4e14bff75be5214c2371215b
AyushSingh360 <ayushsingh21109@gmail.com>
2026-07-27 18:24:56 +0000

    [LeetCode Sync] - Find Largest Value in Each Tree Row - Runtime - 0 ms (100.00%), Memory - 21.1 MB (9.31%)

---
+++ leetcode/0515-find-largest-value-in-each-tree-row/solution.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        ans = []
+        q = deque([root])
+
+        while q:
+            level_max = float('-inf')
+            for _ in range(len(q)):
+                node = q.popleft()
+                level_max = max(level_max, node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            ans.append(level_max)
+
+        return ans
