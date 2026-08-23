54c37b5c040a3d8544c79af148e597809b4a9969
AyushSingh360 <ayushsingh21109@gmail.com>
2026-08-23 02:14:17 +0000

    [LeetCode Sync] - Sum Game - Runtime - 67 ms (29.41%), Memory - 19.9 MB (27.65%)

---
+++ leetcode/2039-sum-game/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        n = len(num) // 2
+
+        left_sum = right_sum = 0
+        left_q = right_q = 0
+
+        for i, ch in enumerate(num):
+            if ch == '?':
+                if i < n:
+                    left_q += 1
+                else:
+                    right_q += 1
+            else:
+                if i < n:
+                    left_sum += int(ch)
+                else:
+                    right_sum += int(ch)
+
+        
+        if (left_q + right_q) % 2 == 1:
+            return True
+
+        
+        return left_sum - right_sum != (right_q - left_q) * 9 // 2
