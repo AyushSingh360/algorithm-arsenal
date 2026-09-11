42e3442623d0088c15468de8f2406df85878dc9c
AyushSingh360 <ayushsingh21109@gmail.com>
2026-09-11 02:26:38 +0000

    [LeetCode Sync] - Unique 3-Digit Even Numbers - Runtime - 11 ms (84.40%), Memory - 19.2 MB (72.53%)

---
+++ leetcode/3799-unique-3-digit-even-numbers/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        valid = set()
+
+        n = len(digits)
+        for i in range(n):
+            if digits[i] == 0:   # no leading zero
+                continue
+
+            for j in range(n):
+                if j == i:
+                    continue
+
+                for k in range(n):
+                    if k == i or k == j:
+                        continue
+
+                    if digits[k] % 2 == 0:
+                        valid.add(digits[i] * 100 + digits[j] * 10 + digits[k])
+
+        return len(valid)
